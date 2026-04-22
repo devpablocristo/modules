e7c7b85a8f7929db9582f82a51f98d7c65db76f3
devpablocristo <devpablocristo@gmail.com>
2026-04-22 08:25:09 -0300

    ok

--- crud/ui/ts/src/index.ts
+++ crud/ui/ts/src/index.ts
@@ -40,6 +40,20 @@ export {
 } from "./strings";
 export { CrudPathSegment, crudItemPath, crudListPath } from "./restPaths";
 export {
+  INTERNAL_FAVORITE_FIELD_KEY,
+  INTERNAL_FAVORITE_LABEL,
+  INTERNAL_NOTES_FIELD_KEY,
+  INTERNAL_NOTES_LABEL,
+  INTERNAL_TAGS_FIELD_KEY,
+  INTERNAL_TAGS_LABEL,
+  buildInternalFavoriteField,
+  buildInternalNotesField,
+  buildInternalTagsField,
+  buildStandardInternalFields,
+  formatTagCsv,
+  parseTagCsv,
+} from "./entityFields";
+export {
   buildCsvToolbarActions,
   mergeCsvToolbarConfig,
   type CrudCsvServerExportPort,
