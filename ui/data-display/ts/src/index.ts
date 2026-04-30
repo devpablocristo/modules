c26ade5bf9819b06cb35c2de2755f33d3d8b1d77
Pablo <devpablocristo@gmail.com>
2026-04-30 17:43:00 -0300

    Add reusable table filters

--- ui/data-display/ts/src/index.ts
+++ ui/data-display/ts/src/index.ts
@@ -3,6 +3,22 @@ export { PaginationBar, type PaginationBarProps } from "./PaginationBar";
 export { SubTable, type SubColumn, type SubTableProps } from "./SubTable";
 export { getTotalPages, usePagination, type UsePaginationOptions } from "./usePagination";
 export {
+  applyTableFilters,
+  getTableFilterOptions,
+  isEmptyTableFilterValue,
+  matchesTableFilterValue,
+  normalizeTableFilterValue,
+  type GetTableFilterOptionsConfig,
+  type TableFilterConfig,
+  type TableFilterMatchMode,
+  type TableFilterValueGetter,
+  type TableFilters,
+} from "./tableFilters";
+export {
+  useClientTableFilters,
+  type UseClientTableFiltersOptions,
+} from "./useClientTableFilters";
+export {
   CursorPager,
   LoadMoreControl,
   type CursorPagerProps,
